0d6d55748114486b52748ac75b634e4dd6669e66
trilophobia <khemissibahja@gmail.com>
2025-07-02 15:23:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-function changeMessage() {
-  document.getElementById("message").textContent = "Main Branch Button Clicked!";
+function generateColor() {
+  const color = "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
+  document.getElementById("colorBox").style.backgroundColor = color;
+  document.getElementById("colorCode").textContent = color;
 }
